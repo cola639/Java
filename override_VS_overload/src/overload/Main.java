e9ab65b34cb19c877ee9b47111d39e28d22b6ab2
cola639 <337507950@qq.com>
2023-08-01 10:51:23 +0800

    add: static

--- override_VS_overload/src/overload/Main.java
+++ override_VS_overload/src/overload/Main.java
@@ -1,11 +1,23 @@
 package overload;
 
 public class Main {
-    public void demoMethod(int a) {
+    public static void demoMethod(int a) {
+        System.out.println("a" + a);
         // some code here
     }
 
-    public void demoMethod(String b) {
+    public static void demoMethod(String b) {
+        System.out.println("b" + b);
         // some code here
+    }
+
+    public  void demoNoStatic(int c) {
+        System.out.println("c" + c);
+        // some code here
+    }
+
+    public static void main(String[] args) {
+        demoMethod("Hello"); // 直接调用静态方法
+        // Error: demoNoStatic(1L);
     }
 }
