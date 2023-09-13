c5c71770959f7f6341858ec8ac1e07f7dcfff3b3
cola639 <337507950@qq.com>
2023-09-14 01:42:09 +0800

    add: thread

--- collection/src/list/Main.java
+++ collection/src/list/Main.java
@@ -1,5 +1,6 @@
 package list;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,7 @@ public class Main {
 
     // 字符串list
     public static void strList() {
-        List<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
         list.add("Hello");
         list.add("World");
         System.out.println("list" + list);
@@ -20,7 +21,7 @@ public class Main {
 
     // 数字list
     public static void numList() {
-        List<Integer> list  = new ArrayList<Integer>();
+        ArrayList<Integer> list  = new ArrayList<Integer>();
         list.add(10);
         list.add(20);
         list.add(30);
@@ -29,7 +30,7 @@ public class Main {
 
     // 对象List
     public static void personList() {
-        List<Person> list = new ArrayList<Person>();
+        ArrayList<Person> list = new ArrayList<Person>();
         list.add(new Person("John", 20));
         list.add(new Person("Jane", 30));
         System.out.println("list" + list);
