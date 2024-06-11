e3724199518da9742c7e30799617c1119ee1e96b
cola639 <337507950@qq.com>
2024-06-11 13:41:56 +0800

    update: doc

--- array/ArrayExample.java
+++ array/ArrayExample.java
@@ -1,10 +1,9 @@
 public class ArrayExample {
     public static void main(String[] args) {
-        // 数组大小
-        int size = 10;
+
         // 创建数组
         // dataType[] arrayRefVar = new dataType[arraySize];
-        double[] myList = new double[size];
+        double[] myList = new double[10];
         myList[0] = 5.6;
         myList[1] = 4.5;
         myList[2] = 3.3;
@@ -15,6 +14,10 @@ public class ArrayExample {
         myList[7] = 45.45;
         myList[8] = 99.993;
         myList[9] = 11123;
+
+        // 多维数组的动态初始化（以二维数组为例）
+        // dataType[][] arrayRefVar = new dataType[arraySize][arraySize];
+        int[][] a = new int[2][3];
 
         // 1 打印所有数组元素
         for (int i = 0; i < myList.length; i++) {
@@ -35,12 +38,11 @@ public class ArrayExample {
         }
         System.out.println("Max is " + max);
 
-        // 4 打印所有数组元素（使用增强for循环）
+        // 4 使用增强for循环 打印所有数组元素（ for : arr形式 ）
         for (double element : myList) {
             System.out.println(element);
         }
 
-        // 示例数组用于函数调用
         int[] exampleArray = {1, 2, 3, 4, 5};
 
         // 5 数组作为函数的参数
@@ -48,10 +50,6 @@ public class ArrayExample {
 
         // 6 数组作为函数的返回值
         int[] reversedArray = reverse(exampleArray);
-        printArray(reversedArray);
-
-        // 7 多维数组的动态初始化（以二维数组为例）
-        int[][] a = new int[2][3];
 
         // 打印二维数组的元素
         for (int i = 0; i < a.length; i++) {
@@ -62,7 +60,7 @@ public class ArrayExample {
         }
     }
 
-    // 5 数组作为函数的参数
+    // 5 作为函数的参数 (将数组作为参数传递给函数)
     public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
@@ -70,7 +68,7 @@ public class ArrayExample {
         System.out.println();
     }
 
-    // 6 数组作为函数的返回值
+    // 6 作为函数的返回值 (将数组作为返回值返回给调用者)
     public static int[] reverse(int[] list) {
         int[] result = new int[list.length];
 
