9ae0f0205438f68561de3f0b5817678952cf0593
gavin <gavinguan@aslcn.com.cn>
2025-03-04 17:30:44 +0800

    update

--- Abstract_VS_Interface/Interface.java
+++ Abstract_VS_Interface/Interface.java
@@ -2,7 +2,7 @@ public interface Interface {
     // 普通接口方法，必须由实现类实现,否则编辑器提示错误
     void method1();
 
-    // 静态方法
+    // 静态方法 只能通过接口来调用
     static void staticMethod() {
         System.out.println("Static method in the interface");
     }
