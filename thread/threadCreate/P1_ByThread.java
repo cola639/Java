9ed2403e0a78bd74ddf2ed78f7e707b6cb1293ee
gavin <gavinguan@aslcn.com.cn>
2025-03-07 17:09:02 +0800

    add main methods to threading examples and implement call method in CallableDemo

--- thread/threadCreate/P1_ByThread.java
+++ thread/threadCreate/P1_ByThread.java
@@ -31,5 +31,13 @@ public class P1_ByThread extends Thread {
             t.start();
         }
     }
+
+    public static void main(String args[]) {
+        P1_ByThread T1 = new P1_ByThread("Thread-1");
+        T1.start();
+
+        P1_ByThread T2 = new P1_ByThread("Thread-2");
+        T2.start();
+    }
 }
 
--- thread/threadCreate/P2_ByRunnableDemo.java
+++ thread/threadCreate/P2_ByRunnableDemo.java
@@ -32,4 +32,12 @@ public class P2_ByRunnableDemo implements Runnable {
         }
     }
 
+    public static void main(String args[]) {
+        P2_ByRunnableDemo R1 = new P2_ByRunnableDemo( "Thread-1");
+        R1.start();
+
+        P2_ByRunnableDemo R2 = new P2_ByRunnableDemo( "Thread-2");
+        R2.start();
+    }
+
 }
