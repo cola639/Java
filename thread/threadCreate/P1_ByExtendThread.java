eba6f52c55aee19cfd71717c32af6580931a3483
guanxh <337507950@qq.com>
2025-03-08 18:35:18 +0800

    doc

--- thread/threadCreate/P1_ByExtendThread.java
+++ thread/threadCreate/P1_ByExtendThread.java
@@ -1,21 +1,23 @@
 package threadCreate;
 
-// extends Thread
+// 继承 Thread
 public class P1_ByExtendThread extends Thread {
-    private Thread t;
     private String threadName;
 
-    P1_ByExtendThread(String name) {
-        threadName = name;
+    // 构造方法
+    public P1_ByExtendThread(String name) {
+        super(name); // 传递线程名
+        this.threadName = name;
         System.out.println("Creating " + threadName);
     }
 
+    // 重写 run() 方法
+    @Override
     public void run() {
         System.out.println("Running " + threadName);
         try {
             for (int i = 4; i > 0; i--) {
                 System.out.println("Thread: " + threadName + ", " + i);
-                // 让线程睡眠一会
                 Thread.sleep(50);
             }
         } catch (InterruptedException e) {
@@ -24,20 +26,14 @@ public class P1_ByExtendThread extends Thread {
         System.out.println("Thread " + threadName + " exiting.");
     }
 
-    public void start() {
-        System.out.println("Starting " + threadName);
-        if (t == null) {
-            t = new Thread(this, threadName);
-            t.start();
-        }
-    }
-
-    public static void main(String args[]) {
+    // 主方法
+    public static void main(String[] args) {
+        // 创建线程实例
         P1_ByExtendThread T1 = new P1_ByExtendThread("Thread-1");
-        T1.start();
-
         P1_ByExtendThread T2 = new P1_ByExtendThread("Thread-2");
+
+        // 启动线程
+        T1.start();
         T2.start();
     }
 }
-
